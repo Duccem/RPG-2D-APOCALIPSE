5c1ffd8f4d45801d018c06101226d0a85358efd2
Duccem <josemanuel376@hotmail.com>
2020-01-02 12:28:52 -0300

    Movement of the character and the animations

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    Vector2 movement;
+    public Rigidbody2D rbd2;
+    public Animator anim;
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        rbd2.MovePosition(rbd2.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if(movement != Vector2.zero)
+        {
+            anim.SetFloat("movX",movement.x);
+            anim.SetFloat("movY",movement.y);
+        }
+        anim.SetFloat("speed",movement.sqrMagnitude);
+    }
+}
