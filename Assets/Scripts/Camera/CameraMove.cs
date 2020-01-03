4617691d8ba0956b610400e4210e27111a9acd10
Duccem <josemanuel376@hotmail.com>
2020-01-03 19:50:24 -0300

    Camera and tilemaps

---
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    
+    public GameObject player; //Player reference to follow him
+    public GameObject initialMap; // Initial map reference to set bounds to the camera movement
+
+    float limitTopLeftX, limitTopLeftY, limitBottomRightX, limitBottomRightY; //Limits on the axis
+    Vector2 velocity;
+    void Awake()
+    {
+        setBounds(initialMap);
+    }
+
+    void FixedUpdate()
+    {
+        //Calculo para el suavizado de la camara
+        float posX = Mathf.Round(Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, 0.5f) * 100)  / 100;
+        float posY = Mathf.Round(Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, 0.5f) * 100) / 100;
+
+        //Movimiento de la camara limitado en fronteras ya definidas
+        transform.position = new Vector3(Mathf.Clamp(posX, limitTopLeftX, limitBottomRightX),
+                                         Mathf.Clamp(posY, limitBottomRightY, limitTopLeftY),
+                                         transform.position.z);
+    }
+
+    public void setBounds(GameObject targetMap)
+    {
+        float ratio = (float)Screen.width / (float)Screen.height;
+        float ajuste = 0f ;
+        if (ratio  >= 1.7f)
+        {
+            ajuste = 1.5f;
+        }
+        else if (ratio == 1.6f)
+        {
+            ajuste = 0.5f;
+        } 
+        MapZone mapa = targetMap.GetComponent<MapZone>();
+        Transform pos = targetMap.GetComponent<Transform>();
+        limitTopLeftX = pos.position.x + (Camera.main.orthographicSize + (Camera.main.orthographicSize / 2) + ajuste);
+        limitTopLeftY = pos.position.y - Camera.main.orthographicSize ;
+        limitBottomRightX = pos.position.x + mapa.TilesWidth -  (Camera.main.orthographicSize + (Camera.main.orthographicSize / 2) + ajuste);
+        limitBottomRightY = pos.position.y - mapa.TilesHeight + Camera.main.orthographicSize;
+    }
+}
---
+++ Assets/Scripts/MapingSystem/MapZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MapZone : MonoBehaviour
+{
+    public int TilesHeight;
+    public int TilesWidth;
+
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 pos = GetComponent<Transform>().position;
+        Vector3 center = new Vector3(pos.x + (TilesWidth / 2), pos.y - (TilesHeight / 2), pos.z);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(center,new Vector3(TilesWidth,TilesHeight,0));
+    }
+
+}
